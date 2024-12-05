abe85fb734bfc17d63dc57a30dcc3a97a8e1c4dc
Zerggh <zerlingtail@gmail.com>
2024-12-05 14:11:18 +0400

    Visual for Okayu

--- Sonduya/Assets/Script/HookOkayu.cs
+++ Sonduya/Assets/Script/HookOkayu.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class HookOkayu : MonoBehaviour
 {
+    public Transform playerPosition;
     public Transform hookPoint; 
     public float ropeLength = 5f;  
     public float maxPullDistance = 8f;  
@@ -12,6 +13,32 @@ public class HookOkayu : MonoBehaviour
 
     private GameObject grabbedObject = null;
     private bool isGrabbing = false;
+
+    private LineRenderer lineRenderer;
+
+    private int layerIndex;
+    void Start()
+    {
+        layerIndex = LayerMask.NameToLayer("Objects");
+
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+    }
+
+    void LateUpdate()
+    {
+        if (isGrabbing && grabbedObject != null)
+        {
+            lineRenderer.SetPosition(0, playerPosition.position);
+            lineRenderer.SetPosition(1, grabbedObject.transform.position);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, playerPosition.position);
+            lineRenderer.SetPosition(1, playerPosition.position);
+        }
+    }
 
     void Update()
     {
@@ -34,7 +61,7 @@ public class HookOkayu : MonoBehaviour
     void TryGrabObject()
     {
         RaycastHit2D hit = Physics2D.Raycast(hookPoint.position, hookPoint.right, maxPullDistance);
-        if (hit.collider != null)
+        if (hit.collider != null && hit.collider.gameObject.layer == layerIndex)
         {
             Rigidbody2D hitRb = hit.collider.GetComponent<Rigidbody2D>();
             if (hitRb != null)
@@ -42,6 +69,10 @@ public class HookOkayu : MonoBehaviour
                 grabbedObject = hit.collider.gameObject;
                 isGrabbing = true;
             }
+        }
+        else
+        {
+            return;
         }
     }
 
@@ -57,12 +88,15 @@ public class HookOkayu : MonoBehaviour
 
     void HandleRope()
     {
-        Vector2 hookPosition = hookPoint.position;
+        Vector2 hookPosition = playerPosition.position;
 
         Vector2 directionToHook = (hookPosition - (Vector2)grabbedObject.transform.position).normalized;
         float distance = Vector2.Distance(hookPosition, grabbedObject.transform.position);
-
-        if (distance > ropeLength)
+        if(distance > maxPullDistance)
+        {
+            ReleaseObject();
+        }
+        else if (distance > ropeLength)
         {
             Rigidbody2D grabbedRb = grabbedObject.GetComponent<Rigidbody2D>();
 
